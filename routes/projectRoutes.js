0ee80440f0e5b48789037e03f6a37b2658124f05
raygdev <101662915+raygdev@users.noreply.github.com>
2025-01-19 23:27:33 -0500

    refactor: remove unused imports

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { Project, User } = require('../models/index.js');
 const ensureAuthenticated = require('../middleware/auth.js');
 const {
     createProject,
