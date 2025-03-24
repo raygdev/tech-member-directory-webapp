7457c82b66cd7ec3f5ed5fc5667aa5b1fd96b6c0
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-24 10:17:20 -0400

    Create js for invalid states on form

---
+++ public/js/verify.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("form");
+const code = document.getElementById("code");
+const feedback = document.querySelector(".invalid-feedback");
+
+const listenerForInput = (text) => {
+  code.addEventListener("input", function (e) {
+    feedback.textContent = text;
+    if (!/^\d{6}$/.test(this.value)) {
+      this.classList.remove("is-valid");
+      this.classList.add("is-invalid");
+      this.setCustomValidity("Invalid");
+    } else {
+      this.classList.remove("is-invalid");
+      this.classList.add("is-valid");
+      feedback.textContent = "";
+      this.setCustomValidity("");
+    }
+  });
+};
+
+if (feedback.textContent.trim() !== "") {
+  code.setCustomValidity("Invalid");
+  listenerForInput(feedback.textContent);
+}
+
+form.addEventListener("submit", function (e) {
+  console.log(code.value);
+  if (!/^\d{6}$/.test(code.value)) {
+    e.preventDefault();
+    form.classList.remove("needs-validation");
+    form.classList.add("was-validated");
+    code.classList.add("is-invalid");
+    code.setCustomValidity("Invalid");
+    feedback.textContent = "Code must be 6 digits";
+    listenerForInput("Code Must Be 6 Digits");
+  }
+});
