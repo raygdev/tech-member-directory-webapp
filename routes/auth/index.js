f7512465e62ff136a635a17680d4acdc42e893e4
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-24 14:14:26 -0400

    Add resend controller to auth routse

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 
 const { postRegister } = require('./register')
+const { postResendVerification } = require('./resend-verification')
 const { getVerifyAccount, postVerifyAccount } = require('./verify-account')
 
 router.post('/register', postRegister),
 router.post('/verify', postVerifyAccount)
 router.get('/verify', getVerifyAccount)
+router.post('/resend', postResendVerification)
 
 module.exports = router
