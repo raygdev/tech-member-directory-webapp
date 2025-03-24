c60a5d963b604995b516a475147abd8225377cb4
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-24 14:14:03 -0400

    Create a resend route for verification code

---
+++ routes/auth/resend-verification.js
@@ -0,0 +1,42 @@
+const { Verification } = require("../../models");
+const transporter = require("../../config/nodemailer.config");
+
+const postResendVerification = async (req, res) => {
+  const { userid } = req.query;
+  const code = Math.floor(100000 + Math.random() * 999999);
+  const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
+  try {
+    await Verification.deleteMany({ user: userid });
+
+    const verificationToSend = new Verification({
+      user: userid,
+      code,
+      expiresAt,
+    });
+
+    await verificationToSend.populate("user");
+    await verificationToSend.save();
+
+    transporter.sendMail(
+      {
+        from: "evan76@ethereal.mail",
+        to: verificationToSend.user.email,
+        subject: "Your verification code",
+        text: `Your code: ${verificationToSend.code}`,
+      },
+      (err, info) => {
+        if (err) throw err;
+
+        console.log("[MAIL SENT INFO]", info);
+        res.json({ message: "A new verification code was sent. Please check your inbox" });
+      }
+    );
+  } catch (error) {
+    console.log("[RESEND VERIFICATION ERROR]", error);
+    res.status(400).json({ message: `An error occurred: ${error.message}` });
+  }
+};
+
+module.exports = {
+  postResendVerification,
+};
