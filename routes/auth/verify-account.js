34fce1af33afaf939122b56c18015e9dfeb83ab9
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-24 10:18:14 -0400

    Change path for rendering verify

--- routes/auth/verify-account.js
+++ routes/auth/verify-account.js
@@ -1,7 +1,7 @@
-const { Verify } = require("../../models/Verify");
+const Verify = require("../../models/Verify");
 
 const getVerifyAccount = async (req, res) => {
-  res.render("verify");
+  res.render("auth/verify");
 };
 
 const postVerifyAccount = async (req, res) => {
@@ -12,7 +12,7 @@ const postVerifyAccount = async (req, res) => {
     const verification = await Verify.findOne({ user: userId });
 
     if (!verification) {
-      res.render("verify", {
+      res.render("auth/verify", {
         error: "Your code appears to be expired",
         code,
       });
@@ -21,7 +21,7 @@ const postVerifyAccount = async (req, res) => {
     }
 
     if (code !== verification.code) {
-      res.render("verify", {
+      res.render("auth/verify", {
         error: "Your code doesn't match",
       });
 
@@ -32,8 +32,13 @@ const postVerifyAccount = async (req, res) => {
     // authenticate the user and redirect to /projects/tables ?
   } catch (error) {
     console.log(error);
-    res.render("verify", {
+    res.render("auth/verify", {
       error: "Something went wrong",
     });
   }
+};
+
+module.exports = {
+  getVerifyAccount,
+  postVerifyAccount,
 };
