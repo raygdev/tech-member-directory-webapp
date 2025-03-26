94f5313860d36892fe7d5720b3b090005139d325
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-26 19:42:41 -0400

    DRY the configuration and reforrmat

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,33 +1,19 @@
 const nodemailer = require("nodemailer");
-const { promisify } = require('util')
+const { promisify } = require("util");
 
-let mailOptions;
+const DEV = process.env.NODE_ENV !== "production";
 
-if (process.env.NODE_ENV !== "production") {
-  mailOptions = {
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000, // timeout after 10 seconds
-    socketTimeout: 20000, // timeout after 20 seconds
-  };
-} else {
-  mailOptions = {
-    host: "smtp.google.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000, // timeout after 10 seconds
-    socketTimeout: 20000 // timeout after 20 seconds
-  };
-}
+const mailOptions = {
+  host: DEV ? "smtp.ethereal.email" : "smtp.google.com",
+  port: DEV ? 587 : 465,
+  secure: DEV ? false : true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000, // timeout after 10 seconds
+  socketTimeout: 20000, // timeout after 20 seconds
+};
 
 const transporter = nodemailer.createTransport(mailOptions);
 
